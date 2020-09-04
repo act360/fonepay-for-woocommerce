cc2f100391f33153adfd6d02c81c6dc88394e134
ACT360 Pvt. Ltd <act360@users.noreply.github.com>
2020-09-04 10:43:22 +0545

    name and slug updated for submission in wp

---
+++ woocommerce-fonepay/woocommerce-fonepay.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: Integrate Fonepay in WooCommerce
+ * Plugin URI: https://github.com/act360/fonepay-for-woocommerce
+ * Description: Integrate Fonepay in WooCommerce is a plugin that enables Fonepay payment in WooCommerce for Nepal.
+ * Version: 1.0.0
+ * Author: ACT360
+ * Author URI: https://act360.com.np
+ * Text Domain: woocommerce-fonepay
+ * Domain Path: /languages
+ *
+ * WC requires at least: 4.3.0
+ * WC tested up to: 5.4.2
+ *
+ * @package Fonepay-WooCommerce
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+// Define WC_FONEPAY_PLUGIN_FILE.
+if ( ! defined( 'WC_FONEPAY_PLUGIN_FILE' ) ) {
+	define( 'WC_FONEPAY_PLUGIN_FILE', __FILE__ );
+}
+
+// Include the main WooCommerce Fonepay class.
+if ( ! class_exists( 'WooCommerce_Fonepay' ) ) {
+	include_once dirname( __FILE__ ) . '/includes/class-woocommerce-fonepay.php';
+}
+
+// Initialize the plugin.
+add_action( 'plugins_loaded', array( 'WooCommerce_Fonepay', 'get_instance' ) );
